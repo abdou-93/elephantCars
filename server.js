b9c121c9f86cc1dd5797f960037de8283c8d0ec8
abdou <abdouforever0@gmail.com>
2018-01-21 02:16:10 +0200

    update get route

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function (req, res) {
-  req.sendFile(path.join(__dirname + 'dist/index.html'));
+  req.sendFile(path.join(__dirname + '/dist/index.html'));
 });
